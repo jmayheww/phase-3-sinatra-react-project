22b0138b2734b825f1463ba33d05cf62bf532f2a
Joshua <joshsmayhew@gmail.com>
2023-01-05 16:09:00 +0700

    create User class methods that sort and filter instances by id, name, creation time, alphabetical order

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,24 @@
 class User < ActiveRecord::Base
   has_many :users_trips
   has_many :trips, through: :users_trips
+
+  def self.find_by_name(name)
+    where(name: name)
+  end
+
+  def self.find_by_id(id)
+    where(id: id)
+  end
+
+  def self.show_in_order_of_creation
+    order(created_at: :desc)
+  end
+
+  def self.show_all_names
+    all.map(&:name)
+  end
+
+  def self.show_all_names_alphabetical
+    show_all_names.sort
+  end
 end
