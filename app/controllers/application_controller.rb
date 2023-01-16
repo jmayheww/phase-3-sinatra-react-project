df3e77b429af1f99973ba0135fc92294b357a6d0
Joshua <joshsmayhew@gmail.com>
2023-01-16 16:37:19 +0700

    add new endpoints in application_controller to facilitate get and push requests for users and userstrips

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < Sinatra::Base
   get '/trips' do
     trips = Trip.show_recently_active_trips.limit(10)
     trips.to_json(only: %i[title budget start_date end_date img id], include: {
-                    users_trips: { only: %i[created_at updated_at], include: :user }
+                    users_trips: { include: :user }
                   })
   end
 
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
     trip = Trip.find(params[:id])
     puts trip
     trip.to_json(only: %i[title budget start_date end_date img id], include: {
-                   users_trips: { only: %i[created_at updated_at], include: :user }
+                   users_trips: { include: :user }
                  })
   end
 
@@ -25,6 +25,23 @@ class ApplicationController < Sinatra::Base
     trip_to_delete = Trip.find(params[:id])
     trip_to_delete.destroy
     trip_to_delete.to_json
+  end
+
+  get '/users' do
+    users = User.show_in_order_of_creation
+    users.to_json(include: :users_trips)
+  end
+
+  get '/users/:id' do
+    user = User.find(params[:id])
+    user.to_json(include: :users_trips)
+  end
+
+  post '/users' do
+    user = User.create(
+      name: params[:name]
+    )
+    user.to_json
   end
 
   post '/userstrips' do
